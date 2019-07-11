9366d6e0e017158c7ba22db7dccbe0503199a5cf
JavaIMMB <electropo4ta2015@yandex.ru>
2019-07-11 16:51:12 +0300

    Webpack + WebSoket

--- src/main/java/letscode/sarafan/controller/MainController.java
+++ src/main/java/letscode/sarafan/controller/MainController.java
@@ -3,6 +3,7 @@ package letscode.sarafan.controller;
 import letscode.sarafan.domain.User;
 import letscode.sarafan.repo.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,6 +18,9 @@ public class MainController {
 
     private final MessageRepo messageRepo;
 
+    @Value("${spring.profiles.active}")
+    private String profile;
+
     @Autowired
     public MainController(MessageRepo messageRepo) {
         this.messageRepo = messageRepo;
@@ -29,6 +33,7 @@ public class MainController {
         data.put("profile", user);
         data.put("messages", messageRepo.findAll());
         model.addAttribute("frontendData", data);
+        model.addAttribute("isDevMode", "dev".equals(profile));
 
         return "index";
     }
