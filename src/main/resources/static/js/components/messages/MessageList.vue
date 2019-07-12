33ac3dea2760f76281cdc647f567011b6966fe34
JavaIMMB <electropo4ta2015@yandex.ru>
2019-07-12 20:42:58 +0300

    Added styles Vuetify

--- src/main/resources/static/js/components/messages/MessageList.vue
+++ src/main/resources/static/js/components/messages/MessageList.vue
@@ -1,13 +1,13 @@
 <template>
-    <div style="position: relative; width: 300px;">
+    <v-layout align-space-around justify-start column>
         <message-form :messages="messages" :messageAttr="message"/>
-        <message-row v-for="message in messages"
+        <message-row v-for="message in sortedMessages"
                      :key="message.id"
                      :message="message"
                      :editMessage="editMessage"
                      :deleteMessage="deleteMessage"
                      :messages="messages"/>
-    </div>
+    </v-layout>
 </template>
 
 <script>
@@ -25,6 +25,11 @@
                 message: null
             }
         },
+        computed: {
+            sortedMessages() {
+                return this.messages.sort((a, b) => -(a.id - b.id))
+            }
+        },
         methods: {
             editMessage(message) {
                 this.message = message
@@ -32,7 +37,7 @@
             deleteMessage(message) {
                 this.$resource('/message{/id}').remove({id: message.id}).then(result => {
                     if (result.ok) {
-                        this.messages.splice(this.messages.indexOf(this.message), 1)
+                        this.messages.splice(this.messages.indexOf(message), 1)
                     }
                 })
             }
--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -2,9 +2,14 @@ import Vue from 'vue'
 import VueResource from 'vue-resource'
 import App from 'pages/App.vue'
 import { connect } from "./util/ws";
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
 
+if (frontendData.profile) {
 connect()
+}
 
+Vue.use(Vuetify)
 Vue.use(VueResource)
 
 new Vue ({
