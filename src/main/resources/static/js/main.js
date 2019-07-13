088d9eda5736cdaccb4852c21201542f50431df4
JavaIMMB <electropo4ta2015@yandex.ru>
2019-07-14 00:13:30 +0300

    Configured Vuex

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify'
+import '@babel/polyfill'
 import 'api/resource'
 import App from 'pages/App.vue'
-import { connect } from "./util/ws";
+import store from 'store/store'
+import { connect } from "./util/ws"
 import 'vuetify/dist/vuetify.min.css'
 
 if (frontendData.profile) {
@@ -13,6 +15,7 @@ Vue.use(Vuetify)
 
 new Vue ({
     el: '#app',
+    store,
     render: a => a(App)
 })
 
